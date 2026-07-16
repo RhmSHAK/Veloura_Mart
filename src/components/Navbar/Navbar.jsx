91c9de9b0708fa1614b8dfd91f0d5f47e3ed47af
RhmSHAk <mehedi697864@gmail.com>
2026-07-17 03:01:41 +0600

    brand logo add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router';
 import useCart from '../../hook/useCart';
 
 const Navbar = () => {
-    const { totalItems,totalPrice } = useCart();
+    const { totalItems, totalPrice } = useCart();
 
-    const navItems=    <>
-                            <li className='hover:bg-blue-500'><Link to='/' >Home</Link></li>
-                            <li className='hover:bg-blue-500'><Link to='/products' >Products</Link></li>
-                            <li className='hover:bg-blue-500'><Link to='/cart' >Cart</Link></li>
-                    
-                          </>
+    const navItems = <>
+        <li className='hover:bg-blue-500'><Link to='/' >Home</Link></li>
+        <li className='hover:bg-blue-500'><Link to='/products' >Products</Link></li>
+        <li className='hover:bg-blue-500'><Link to='/cart' >Cart</Link></li>
+
+    </>
 
     return (
         <div className="navbar bg-blue-200 shadow-sm sticky top-0 z-[9999]">
@@ -25,8 +25,22 @@ const Navbar = () => {
                         {navItems}
                     </ul>
                 </div>
-                <Link to='/' className="btn  hover:bg-blue-500 btn-ghost text-xl">VeLoUrA MART</Link>
+                
+              {/* logo & brand name */}
+                <Link
+                    to="/"
+                    className="btn btn-ghost hover:bg-blue-500 text-xl flex items-center gap-2"
+                >
+                    <img
+                        src="/logo.png"
+                        alt="Veloura Mart Logo"
+                        className="w-10 h-10 object-contain"
+                    />
+                    <span>VeLoUrA MART</span>
+                </Link>
+
             </div>
+
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {navItems}
@@ -35,25 +49,25 @@ const Navbar = () => {
             {/* --------------------- */}
 
             <div className="navbar-end">
-            <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-blue-500 btn-circle">
-                    <div className="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-                        <span className="badge badge-sm indicator-item">{totalItems}</span>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-blue-500 btn-circle">
+                        <div className="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                            <span className="badge badge-sm indicator-item">{totalItems}</span>
+                        </div>
                     </div>
-                </div>
-                <div
-                    tabIndex={0}
-                    className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
-                    <div className="card-body">
-                        <span className="text-lg font-bold">{totalItems} Items</span>
-                        <span className="text-info">Subtotal: ${totalPrice}</span>
-                        <div className="card-actions">
-                            <Link to="/cart" className="btn btn-primary btn-block">View cart</Link>
+                    <div
+                        tabIndex={0}
+                        className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+                        <div className="card-body">
+                            <span className="text-lg font-bold">{totalItems} Items</span>
+                            <span className="text-info">Subtotal: ${totalPrice}</span>
+                            <div className="card-actions">
+                                <Link to="/cart" className="btn btn-primary btn-block">View cart</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     );
